ee16c990b5e195b70d1727dd08be67acd12ede67
benjamin-angel <ben_angel_1@yahoo.com.au>
2022-04-01 12:26:44 +1100

    Add React Chakra-ui to project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { ChakraProvider, Box, theme } from '@chakra-ui/react';
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign='center' fontSize='xl'></Box>
+  </ChakraProvider>
+);
