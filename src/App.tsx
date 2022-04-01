3262232fc402bb13aea70e4e2ef1fe1186ede0f0
benjamin-angel <ben_angel_1@yahoo.com.au>
2022-04-01 15:59:31 +1100

    Add pokeapi fetch and display to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,50 @@
-import { ChakraProvider, Box, theme } from '@chakra-ui/react';
+import { ChakraProvider, Box, theme, Button } from '@chakra-ui/react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import PokemonDisplay from './components/PokemonDisplay';
+import SearchBar from './components/SearchBar';
+import { PokeApiResponse } from './interfaces/pokemon';
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign='center' fontSize='xl'></Box>
-  </ChakraProvider>
-);
+export const App = () => {
+  const [pokemonObject, setPokemonObject] = useState<PokeApiResponse | null>(
+    null
+  );
+  const [numPokemonSelected, setNumPokemonSelected] = useState(20);
+
+  useEffect(() => {
+    console.log('This still works');
+    const getPokemonInfo = async () => {
+      const pokemonResponse: PokeApiResponse = await axios
+        .get(`https://pokeapi.co/api/v2/pokemon?limit=${numPokemonSelected}`)
+        .then((response) => response.data)
+        .catch((err: Error) =>
+          console.log(`Error getting pokemon response: ${err.message}`)
+        );
+
+      if (!pokemonResponse) return;
+      console.log('pokemonResponse: ', pokemonResponse);
+
+      setPokemonObject(pokemonResponse);
+    };
+
+    getPokemonInfo();
+  }, [numPokemonSelected]);
+
+  const handleNumPokemonSelected = (num: number) => {
+    setNumPokemonSelected(num);
+  };
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign='center' fontSize='xl'>
+        <SearchBar handleNumPokemonSelected={handleNumPokemonSelected} />
+        {pokemonObject ? (
+          <PokemonDisplay results={pokemonObject.results} />
+        ) : (
+          ''
+        )}
+        <Button>Load More</Button>
+      </Box>
+    </ChakraProvider>
+  );
+};
